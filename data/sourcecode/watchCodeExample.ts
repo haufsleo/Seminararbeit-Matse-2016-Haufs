a9d175e7437597a40c02047275098947d0633a9c
haufsleo <haufsleo@users.noreply.github.com>
2016-12-12 18:37:41 +0100

    Stand nach der Verbesserung der Anmerkungen von Olli

--- data/sourcecode/watchCodeExample.ts
+++ data/sourcecode/watchCodeExample.ts
@@ -1,11 +1,14 @@
-/* Die in XCode festgelegte appGroupId "evenAppGroup" und die Variable
-   successfullWatchInit sind global verfuegbar oder ueber einen 
-   entsprechenden Mechanismus (z.B. local storage) global erreichbar. */
-var appGroupId = "evenAppGroup";
+/* 
+Die in XCode festgelegte appGroupId "eventAppGroup" und die Variable
+successfullWatchInit sind global verfuegbar oder ueber einen 
+entsprechenden Mechanismus (z.B. local storage) global erreichbar. 
+*/
+var appGroupId = "eventAppGroup";
 var successfullWatchInit = false;
-// Bei Appstart wird aufgerufen:
+
+/* Bei Appstart wird aufgerufen: */
 var initPlattformAccess = () => {
-    if (this.platform.is('ios')) { 
+    if (this.platform.is('ios')) {
         applewatch.init(() => {
             // success
             successfullWatchInit = true;
@@ -15,16 +18,19 @@ var initPlattformAccess = () => {
         }, appGroupId);
     }
 }
-/* Definiton der zu sendenden Daten als JSON-Objekt */
+
+/* Definition der zu sendenden Daten als Array von JSON-Objekten */
 var data = [{
-        type: 'talk',
-        title: 'Keynote',
-        startDate: (new Date(2017, 1, 14, 08, 0, 0)).toISOString(),
-        endDate: (new Date(2017, 1, 14, 10, 0, 0)).toISOString(),
-        talkerName: 'Dr. Karl Mustermann',
-        location: 'Grosser Hauptsaal',
-        isFavorite: true
-    }, /* ... */ ];
+    type: 'talk',
+    title: 'Keynote',
+    startDate: (new Date(2017, 1, 14, 08, 0, 0)).toISOString(),
+    endDate: (new Date(2017, 1, 14, 10, 0, 0)).toISOString(),
+    talkerName: 'Dr. Karl Mustermann',
+    location: 'Grosser Hauptsaal',
+    isFavorite: true
+}, /* ... */
+];
+
 /* Senden an die Apple Watch als Teil der User Defaults */
 if (successfullWatchInit) {
     applewatch.sendUserDefaults(() => {
